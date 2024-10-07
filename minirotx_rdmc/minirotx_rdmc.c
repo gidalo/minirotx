124b566f513346db6ca4053f301b020ed31133c0
MatixVision <46072070+gidalo@users.noreply.github.com>
2024-10-07 15:52:22 +0200

    Update minirotx_rdmc.c

--- minirotx_rdmc/minirotx_rdmc.c
+++ minirotx_rdmc/minirotx_rdmc.c
@@ -1,12 +1,12 @@
 /* 
 * MiniRotx RDMC "Rotate Display Motor Control" Ver.1.0beta
-* for use stepper motor and driver
+* for use stepper motor 28BYJ-48 and driver ULN2003
 * By Salvatore Balestrieri (MatixVision) oct2024 
 * e-mail:matixvision@gidalo.it
 * youtube: @matixvision
 * License: GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 * Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
-* 
+* Compile and use only Raspberry Pi zero W2 
 * sudo apt-get update
 * sudo apt install -y wiringpi
 * gpio -v
