981f52cff8b8c7846e85f1d6e15c3fe3e5742c89
MatixVision <46072070+gidalo@users.noreply.github.com>
2024-10-21 21:32:17 +0200

    Update JoyMiniRotx.ino

--- JoyMiniRotx/JoyMiniRotx.ino
+++ JoyMiniRotx/JoyMiniRotx.ino
@@ -12,7 +12,7 @@
  * License: GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
  * Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
  *
- * credit lib: 
+ * Credit lib: 
  * https://github.com/MHeironimus/ArduinoJoystickLibrary
  * https://github.com/MHeironimus/ArduinoJoystickLibrary/archive/master.zip
  * ArduinoJoystickLibrary
