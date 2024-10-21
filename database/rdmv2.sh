a5e53c11c9f7176754f50ff3e06ad3e214ff8a01
MatixVision <46072070+gidalo@users.noreply.github.com>
2024-10-21 22:18:28 +0200

    Update rdmv2.sh

--- database/rdmv2.sh
+++ database/rdmv2.sh
@@ -1,6 +1,14 @@
 
 #!/bin/bash
 
+# For MiniRotx RDMC "Rotate Display Motor Control" Ver.1.0beta
+# for use stepper motor 28BYJ-48 and driver ULN2003
+# By Salvatore Balestrieri (MatixVision) oct2024 
+# e-mail:matixvision@gidalo.it
+# youtube: @matixvision
+# License: GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+# Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+
 # File di log da monitorare
 LOG_FILE="/dev/shm/runcommand.log"
 DB_FILE="/home/pi/database/arcade_orientation.txt"
