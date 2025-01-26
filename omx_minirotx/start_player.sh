2b40742dcedfe2fd151b9c6f8b013ba8f4126edf
MatixVision <46072070+gidalo@users.noreply.github.com>
2025-01-26 15:10:59 +0100

    Update start_player.sh

--- omx_minirotx/start_player.sh
+++ omx_minirotx/start_player.sh
@@ -2,8 +2,6 @@
 
 # Avvia il programma joypad
 sudo /opt/retropie/supplementary/movies/omx_minirotx_v6
-#./omx_v2 &
-
 # Aspetta che il joypad avvii il film
 echo "In attesa che il joypad controlli omxplayer..."
 wait
